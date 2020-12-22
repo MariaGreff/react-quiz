b834dcdd08a7f3941fa76256ea953593962c406f
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-12-22 20:25:48 +0100

    created input component

---
+++ src/components/Ui/Input/Input.js
@@ -0,0 +1,20 @@
+import classes from "./Input.module.scss";
+
+function Input (props) {
+  const inputType = props.type || 'text';
+  const cls = [classes["Input"]];
+  const htmlFor = `${inputType}-${Math.random()}`
+  return (
+    <div className={cls.join(' ')}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input 
+        type={inputType}
+        id={htmlFor}
+        value={props.value}
+        onChange ={props.onChange}
+       />
+    </div>
+  )
+};
+
+export default Input;
