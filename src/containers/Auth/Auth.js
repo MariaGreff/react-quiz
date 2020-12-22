a7ceed014e372708e684ffa9fcb745a48142e932
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-12-22 12:46:24 +0100

    created authorization component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,39 @@
+import Button from "../../components/Ui/Button/Button";
+import classes from "./Auth.module.scss";
+
 function Auth () {
+
+const loginHandler = () => {
+
+};
+
+const registerHandler = () => {
+
+};
+
+const submitHandler = event => {
+  event.preventDefault();
+};
+
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className={classes["Auth"]}>
+      <div>
+        <h1>Authorization</h1>
+
+        <form className={classes["AuthForm"]} onSubmit={submitHandler}>
+          <input type="text" />
+          <input type="text" />
+          <Button
+            type="success"
+            onClick={loginHandler}
+          >
+          Log In</Button>
+          <Button
+            type="primary"
+            onClick={registerHandler}
+          >Sign In</Button>
+        </form>
+      </div>
     </div>
   );
 };
